bd8c906eebddb770d8882fe2f7c831df6125565e
Samuel <dev.samuelsabinoperira@gmail.com>
2024-01-07 21:33:32 -0300

    Add temporirador para remover a tag jump

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,12 @@ const mario = document.querySelector('.mario');
 
 const jump1 = () => { 
     mario.classList.add('jump'); 
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump'); 
+
+    },550);
 } 
 
 document.addEventListener('keydown', jump1);
