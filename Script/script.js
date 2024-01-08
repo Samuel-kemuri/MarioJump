f4860423a79030e014cedc1b83edadd824b1d521
Samuel <dev.samuelsabinoperira@gmail.com>
2024-01-07 23:53:55 -0300

    Adcionado empacto do cano

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump1 = () => { 
     mario.classList.add('jump'); 
@@ -9,5 +10,18 @@ const jump1 = () => {
 
     },550);
 } 
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+
+    if (pipePosition <= 126){
+
+        pipe.style.animation = 'nome';
+        pipe.style.left = `${pipePosition}px`;
+
+    }
+
+} , 10)
 
 document.addEventListener('keydown', jump1);
